fec6f7d3fa44eeaaf073042fb67a5614d38c37a6
pixel-tom <bnorris2710@outlook.com>
2024-03-14 14:40:12 -0500

    update image loading

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "847d43f28c56a8f5"; }
+/******/ 		__webpack_require__.h = function() { return "9957b3f80c589597"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/DogePreview.tsx
+++ components/DogePreview.tsx
@@ -1,10 +1,18 @@
-// components/CharacterPreview.tsx
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import NextImage from "next/image";
 import { CharacterParts, SelectedCharacterParts } from "../types";
 import { characterParts } from "@/data";
 import DownloadIcon from '@mui/icons-material/Download';
 import ShuffleIcon from '@mui/icons-material/Shuffle';
+
+// Loading spinner component
+const LoadingSpinner = () => {
+  return (
+    <div className="absolute top-0 left-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+    </div>
+  );
+};
 
 interface Props {
   selectedParts: SelectedCharacterParts;
@@ -17,72 +25,79 @@ const CharacterPreview: React.FC<Props> = ({
   onRandomize,
   previewRef,
 }) => {
-  const isMobileDevice = () => {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      window.navigator.userAgent
-    );
-  };
+  const [loading, setLoading] = useState(false);
 
   const handleDownload = async () => {
-    const selectedPartKeys = Object.keys(selectedParts);
-    if (selectedPartKeys.length === 0) return; // No selected parts
-  
-    const canvas = document.createElement("canvas");
-    const context = canvas.getContext("2d");
-  
-    const imagePromises = selectedPartKeys.map((category) => {
-      const part = selectedParts[category];
-      return new Promise<HTMLImageElement>((resolve, reject) => {
-        const image = new Image();
-        image.crossOrigin = "anonymous";
-        image.onload = () => {
-          resolve(image);
-        };
-        image.onerror = reject;
-        image.src = part.image;
+    setLoading(true);
+
+    // Simulate asynchronous download process
+    setTimeout(() => {
+      setLoading(false);
+      const canvas = document.createElement("canvas");
+      const context = canvas.getContext("2d");
+      const selectedPartKeys = Object.keys(selectedParts);
+      if (selectedPartKeys.length === 0) return; // No selected parts
+    
+      const imagePromises = selectedPartKeys.map((category) => {
+        const part = selectedParts[category];
+        return new Promise<HTMLImageElement>((resolve, reject) => {
+          const image = new Image();
+          image.crossOrigin = "anonymous";
+          image.onload = () => {
+            resolve(image);
+          };
+          image.onerror = reject;
+          image.src = part.image;
+        });
       });
-    });
-  
-    try {
-      const images = await Promise.all(imagePromises);
-  
-      canvas.width = images[0].width;
-      canvas.height = images[0].height;
-  
-      images.forEach((image) => {
-        context?.drawImage(image, 0, 0);
-      });
-  
-      canvas.toBlob((blob) => {
-        if (blob) {
-          const url = URL.createObjectURL(blob);
-          const link = document.createElement("a");
-          link.href = url;
-          link.download = "character.png";
-          link.click();
-          URL.revokeObjectURL(url);
-        }
-      }, "image/png");
-    } catch (error) {
-      console.error("Image download failed:", error);
-    }
+    
+      try {
+        Promise.all(imagePromises).then(images => {
+          canvas.width = images[0].width;
+          canvas.height = images[0].height;
+    
+          images.forEach((image) => {
+            context?.drawImage(image, 0, 0);
+          });
+    
+          canvas.toBlob((blob) => {
+            if (blob) {
+              const url = URL.createObjectURL(blob);
+              const link = document.createElement("a");
+              link.href = url;
+              link.download = "character.png";
+              link.click();
+              URL.revokeObjectURL(url);
+            }
+          }, "image/png");
+        });
+      } catch (error) {
+        console.error("Image download failed:", error);
+      }
+    }, 1000);
   };
-  
 
   const handleRandomize = () => {
-    const newSelectedParts: SelectedCharacterParts = {};
-    for (const category in characterParts) {
-      const parts = characterParts[category as keyof CharacterParts];
-      const randomIndex = Math.floor(Math.random() * parts.length);
-      newSelectedParts[category as keyof CharacterParts] = parts[randomIndex];
-    }
-    onRandomize(newSelectedParts);
+    setLoading(true);
+
+    // Simulate asynchronous randomization process
+    setTimeout(() => {
+      setLoading(false);
+      const newSelectedParts: SelectedCharacterParts = {};
+      for (const category in characterParts) {
+        const parts = characterParts[category as keyof CharacterParts];
+        const randomIndex = Math.floor(Math.random() * parts.length);
+        newSelectedParts[category as keyof CharacterParts] = parts[randomIndex];
+      }
+      onRandomize(newSelectedParts);
+    }, 1000);
   };
 
   return (
     <div>
+      {loading && <LoadingSpinner />} {/* Show loading spinner when loading */}
       <div
-        className="relative mx-auto h-[300px] w-[300px] md:h-78 md:h-78 lg:h-[300px] lg:w-[300px] xl:h-[320px] xl:w-[320px] bg-none rounded-sm shadow-sm mb-2"
+        className={`relative mx-auto h-[300px] w-[300px] md:h-78 md:h-78 lg:h-[300px] lg:w-[300px] xl:h-[320px] xl:w-[320px] bg-none rounded-sm shadow-sm mb-2 ${loading && 'opacity-30 shadow-inner'}`}
         ref={previewRef}
       >
         {Object.keys(selectedParts).map((category) => {
