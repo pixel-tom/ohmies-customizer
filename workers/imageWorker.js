2fc1a56302662d407767e01047497968c8ce2d83
pixel-tom <pixeltom0x@icloud.com>
2024-05-21 23:47:24 -0500

    update downloading functionality

--- workers/imageWorker.js
+++ workers/imageWorker.js
@@ -1,34 +1,31 @@
 self.onmessage = async (event) => {
-    const { selectedParts } = event.data;
-  
-    const canvas = new OffscreenCanvas(1000, 1000);
-    const context = canvas.getContext("2d");
-    const selectedPartKeys = Object.keys(selectedParts);
-  
-    try {
-      const imagePromises = selectedPartKeys.map((category) => {
-        const part = selectedParts[category];
-        const image = new Image();
-        image.crossOrigin = "anonymous";
-        image.src = part.image;
-        return new Promise((resolve, reject) => {
-          image.onload = () => resolve(image);
-          image.onerror = reject;
-        });
-      });
-  
-      const images = await Promise.all(imagePromises);
-      canvas.width = images[0].width;
-      canvas.height = images[0].height;
-  
-      images.forEach((image) => {
-        context.drawImage(image, 0, 0);
-      });
-  
-      const blob = await canvas.convertToBlob({ type: "image/png" });
-      self.postMessage({ blob });
-    } catch (error) {
-      self.postMessage({ error: error.message });
-    }
-  };
-  
+  const { selectedParts } = event.data;
+
+  const canvas = new OffscreenCanvas(1000, 1000);
+  const context = canvas.getContext("2d");
+  const selectedPartKeys = Object.keys(selectedParts);
+
+  try {
+    const imagePromises = selectedPartKeys.map(async (category) => {
+      const part = selectedParts[category];
+      const response = await fetch(part.image, { mode: 'cors' });
+      if (!response.ok) throw new Error('Failed to load image');
+      const blob = await response.blob();
+      const imageBitmap = await createImageBitmap(blob);
+      return imageBitmap;
+    });
+
+    const images = await Promise.all(imagePromises);
+    canvas.width = images[0].width;
+    canvas.height = images[0].height;
+
+    images.forEach((image) => {
+      context.drawImage(image, 0, 0);
+    });
+
+    const blob = await canvas.convertToBlob({ type: 'image/png' });
+    self.postMessage({ blob });
+  } catch (error) {
+    self.postMessage({ error: error.message });
+  }
+};
